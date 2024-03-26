0a86c6322be92670d2c5410caf9ab35450a48cb3
Hilan Meyran <hilanmeyran@protonmail.com>
2024-03-26 19:31:15 +0100

    cleaned out main

--- aes_grinder/src/main.rs
+++ aes_grinder/src/main.rs
@@ -24,17 +24,4 @@ impl GlobalInfos {
 fn main() {
     let mut cli = cli::Cli::parse();
     cli::cli_check(&mut cli);
-
-    let mut globals: GlobalInfos = GlobalInfos::new(cli.equation_system);
-    let mut parser_mod = Parser::new(&globals);
-
-    let matrix = Matrix::new(2, 2);
-    println!("{}", matrix);
-    let matrix_just_parsed = parser_mod.parse_system(&mut globals).unwrap();
-    let matrix:Matrix = matrix_just_parsed.into();
-
-    print!("{:?}", matrix);
-
-    // parser::parser (cli.equation_system);
-    // Continued program logic goes here...
 }

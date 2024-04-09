928d8474c9a47015788fa28d78fb7cf5cfac85d3
Samuel MAUGER <smauger.fr>
2024-04-09 14:31:12 +0100

    Fix exhaustive search Tofix update_queue

--- aes_grinder/src/exaustive_search.rs
+++ aes_grinder/src/exaustive_search.rs
@@ -1,37 +1,72 @@
-use crate::matrix::Matrix;
-use std::collections::HashSet;
+use crate::{algo::Algo, matrix::Matrix};
+use std::{cmp::Ordering, collections::HashSet};
 
-fn exhaustive_search<E>(x: Matrix, time_complexity: i32) -> Vec<Algo> {
+fn exhaustive_search<E>(x: Matrix, time_complexity: u32) -> HashSet<Box<Algo>> {
     //Set of base solvers
-    let mut g:HashSet<Algo> = HashSet::new();
-    for xVar in x.vars_map.keys() {
-        //We create a base solver for each variable présente
-        gVar.insert(Algo::base_solver(&x, xVar, 256));
+    let mut g:HashSet<Box<Algo>> = HashSet::new();
+    for xVar in x.get_all_variables() {
+        //We create a base solver for each variables for variables that are not S(x)
+        if xVar.contains('(') {
+            continue;
+        }
+        g.insert(Box::new(Algo::base_solver(&x, xVar)));
     }
     //Set of pair of algo
-    let mut p:HashSet<(Algo, Algo)> = HashSet::new();
-    
-    for (i, item1) in g.iter().enumerate() {
-            for item2 in g.iter().enumerate() {
-            p.insert((item1, item2));
-        }
-    }
-    
+    let mut p:HashSet<(Box<Algo>, Box<Algo>)> = HashSet::new();
+    g.clone().iter().enumerate().for_each(|(i, a1)| {
+        g.clone().iter().enumerate().for_each(|(j, a2)| {
+            if i < j {
+                p.insert((a1.clone(), a2.clone()));
+            }
+        });
+    });    
 
     while !p.is_empty() {
-        let (a1, a2) = p.pop();
-        let c = fusion_two_algo(a1, a2);
+        //Take a pair of algo from p
+        let (a1, a2) = p.iter().next().unwrap();
+        let c = Box::new(Algo::fusion_two_algo(a1.clone(), a2.clone()));
 
-        if c.time <= time_complexity {
-            let (new_g, new_p) = update_queue(g, p, c);
-            g = new_g;
-            p = new_p;
+        if c.get_time_complexity() <= time_complexity {
+            update_queue(&mut g, &mut p, c);
         }
-    }
-    
+    };
     g
 }
 
-fn update_queue(g:HashSet<Algo>, p:HashSet<(Algo, Algo)>, c:Algo) -> (HashSet<Algo>, HashSet<(Algo, Algo)>){
-    (GPrime, PPrime)
+fn update_queue(g:&mut HashSet<Box<Algo>>, p:&mut HashSet<(Box<Algo>, Box<Algo>)>, c:Box<Algo>) -> () {
+    //For all algo worst than c
+    for a in g.iter() {
+        match c.compare1(a) {
+            Some(Ordering::Greater) => {
+                let mut gdiff: HashSet<Box<Algo>> = g.iter().fold(HashSet::<Box<Algo>>::new(), |mut s, elt| -> HashSet<Box<Algo>> {
+                    if c.compare1(elt) == Some(Ordering::Greater) {
+                        s.insert(elt.clone());
+                    }
+                    s
+                });
+                g.insert(c.clone());
+                let gprim = g.difference(&mut gdiff).cloned().collect::<HashSet<Box<Algo>>>();
+                let mut pdiff = p.iter().fold(HashSet::<(Box<Algo>, Box<Algo>)>::new(), |mut s, elt| -> HashSet<(Box<Algo>, Box<Algo>)> {
+                    if c.compare1(&elt.0) == Some(Ordering::Greater) || c.compare1(&elt.1) == Some(Ordering::Greater) {
+                        s.insert(elt.clone());
+                    }
+                    s
+                });
+                let pprim = p.difference(&mut pdiff).cloned().collect::<HashSet<(Box<Algo>, Box<Algo>)>>();
+                let uprim =  pprim.union(&mut gprim.iter().fold(HashSet::<(Box<Algo>, Box<Algo>)>::new(), |mut s, elt| -> HashSet<(Box<Algo>, Box<Algo>)> {
+                    let vc = c.get_all_variables();
+                    let velt = elt.get_all_variables();
+                    if !vc.is_subset(&velt) && !velt.is_subset(&vc) {
+                        s.insert((c.clone(), elt.clone()));
+                    }
+                    s
+                })).cloned().collect::<HashSet<(Box<Algo>, Box<Algo>)>>();
+                //Find a means to update g and p
+                *g = gprim;
+                *p = uprim;
+                todo!();
+            },
+            _ => continue,
+        }
+    }
 } 

6deb3167484518174a5faa5103c95a11778b3aa4
Samuel <samuel@smauger.fr>
2024-04-30 17:19:40 +0200

    New exhaustive search version

--- aes_grinder/src/exaustive_search.rs
+++ aes_grinder/src/exaustive_search.rs
@@ -1,8 +1,8 @@
 use crate::{algo::Algo, matrix::Matrix};
+use rand::{seq::IteratorRandom, Rng};
 use std::{cmp::Ordering, collections::HashSet};
-use rand::Rng;
 
-pub fn random_search (mut matrix: Matrix) -> Box<Algo> {
+pub fn random_search(mut matrix: Matrix) -> Box<Algo> {
     //Set of base solvers
     let mut lst_algo: Vec<Box<Algo>> = vec![];
 
@@ -15,14 +15,17 @@ pub fn random_search (mut matrix: Matrix) -> Box<Algo> {
     }
 
     let mut size_of_lst = lst_algo.len();
-    while  size_of_lst > 1 {
-        let first_rand  = rand::thread_rng().gen_range(0..size_of_lst);
+    while size_of_lst > 1 {
+        let first_rand = rand::thread_rng().gen_range(0..size_of_lst);
         size_of_lst -= 1;
         let second_rand = rand::thread_rng().gen_range(0..size_of_lst);
         let first_algo = lst_algo.remove(first_rand);
         let second_algo = lst_algo.remove(second_rand);
-        lst_algo.push(Box::new(Algo::fusion_two_algo(first_algo,
-            second_algo, &mut matrix)));   
+        lst_algo.push(Box::new(Algo::fusion_two_algo(
+            first_algo,
+            second_algo,
+            &mut matrix,
+        )));
     }
 
     lst_algo.pop().unwrap()
@@ -38,6 +41,8 @@ pub fn exhaustive_search(mut x: Matrix, time_complexity: usize) -> HashSet<Box<A
         }
         g.insert(Box::new(Algo::base_solver(&mut x, x_var)));
     }
+    println!("G init");
+    print_algo_set(&g);
     //Set of pair of algo
     let mut p: HashSet<(Box<Algo>, Box<Algo>)> = HashSet::new();
     g.clone().iter().enumerate().for_each(|(i, a1)| {
@@ -49,22 +54,46 @@ pub fn exhaustive_search(mut x: Matrix, time_complexity: usize) -> HashSet<Box<A
             }
         });
     });
-
-    while !p.is_empty() {
+    println!("P init");
+    print_pair_algo_set(&p);
+    let mut rng = rand::thread_rng();
+    // While g dont contains an algo with 20 variables
+    while !g.iter().fold(false, |b, elt| b || elt.get_all_variables().len() == 20) {
         //Take a pair of algo from p
-        print!(" plen : {:?}", p.len());
-        let a = p.iter().next().unwrap();
-        let (a1, a2) = p.take(&a.clone()).unwrap();
+        //println!(" plen : {:?}", p.len());
+        let a = p.iter().choose(&mut rng).unwrap().clone();
+        let (a1, a2) = p.take(&a).unwrap();
 
-        // println!("take : {:?}\n{:?}", a2.clone(),a1.clone());
-        // println!("plen : {:?}", p.len());
+        //println!("take : \n{:?} and {:?}", a1.get_all_variables(),a2.get_all_variables());
         let c = Box::new(Algo::fusion_two_algo(a1.clone(), a2.clone(), &mut x));
-
+        //println!("New algo : \n{:?}", c);
         if c.get_time_complexity() <= time_complexity {
             update_queue(&mut g, &mut p, c);
+        } else {
+            println!("Time complexity reached")
         }
+        //print_algo_set(&g);
+        //print_pair_algo_set(&p);
     }
     g
+}
+
+fn print_algo_set(g: &HashSet<Box<Algo>>) {
+    println!("g ({}):", g.len());
+    g.iter().for_each(|a| {
+        println!("algo : {:?}", a.get_all_variables());
+    });
+}
+
+fn print_pair_algo_set(p: &HashSet<(Box<Algo>, Box<Algo>)>) {
+    println!("p ({}):", p.len());
+    p.iter().for_each(|(a1, a2)| {
+        println!(
+            "Pair : ({:?}, {:?})",
+            a1.get_all_variables(),
+            a2.get_all_variables()
+        );
+    });
 }
 
 // function UPDATE-QUEUE(G, P, A):
@@ -76,67 +105,48 @@ pub fn exhaustive_search(mut x: Matrix, time_complexity: usize) -> HashSet<Box<A
 //     return (G', P')
 // end function
 fn update_queue(g: &mut HashSet<Box<Algo>>, p: &mut HashSet<(Box<Algo>, Box<Algo>)>, c: Box<Algo>) {
-    let mut dominated = false;
-    g.iter().for_each(|a| {
-        let cmp = (*c).partial_cmp(a.as_ref());
-        if cmp == Some(Ordering::Greater)  {
-            dominated = true;
+    //Check if there exists an Algo worth than c
+    for aprim in g.iter() {
+        if !(aprim >= &c) {
+            continue;
+        } else {
+            return;
         }
-        print!("  dominné {:?} : {}\n",cmp, dominated);
+    }
+
+    // Keep all algo that are better or equal to c
+    g.retain(|elt| {
+        if Some(Ordering::Less) == elt.compare1(&c) {
+            false
+        } else {
+            true
+        }
     });
 
-    if !dominated {
-        // Calculate G' as {A} union G minus {A' in G: A dominates A'}
-        let gdiff: HashSet<Box<Algo>> = g.iter().fold(
-            HashSet::<Box<Algo>>::new(),
-            |mut s, elt| -> HashSet<Box<Algo>> {
-                if c.compare1(elt) == Some(Ordering::Greater) {
-                    s.insert(elt.clone());
-                }
-                s
-            },
-        );
+    // Add the new algo to the set
+    g.insert(c.clone());
 
-        // Calculate G' as {A} union G minus {A' in G: A dominates A'}
-        let gprim: HashSet<Box<Algo>> = g.difference(&gdiff).cloned().collect();
+    // Keep all pair that are better or equal to c
+    p.retain(|(a1, a2)| {
+        if a1.compare1(&c) == Some(Ordering::Less) || a2.compare1(&c) == Some(Ordering::Less)
+        {
+            false
+        } else {
+            true
+        }
+    });
 
-        // Calculate P' as P minus {(A1, A2) in P: A dominates A1 or A dominates A2}
-        let pdiff = p.iter().fold(
-            HashSet::<(Box<Algo>, Box<Algo>)>::new(),
-            |mut s, elt| -> HashSet<(Box<Algo>, Box<Algo>)> {
-                if c.compare1(&elt.0) == Some(Ordering::Greater)
-                    || c.compare1(&elt.1) == Some(Ordering::Greater)
-                {
-                    s.insert(elt.clone());
-                }
-                s
-            },
-        );
-        let pprim = p
-            .difference(&pdiff)
-            .cloned()
-            .collect::<HashSet<(Box<Algo>, Box<Algo>)>>();
-
-        // Calculate P' as P' union {(A, A'): A' in G', vertices of A not in vertices of A' and vertices of A' not in vertices of A}
-        let uprim: HashSet<(Box<Algo>, Box<Algo>)> = pprim
-            .union(&gprim.iter().fold(
-                HashSet::<(Box<Algo>, Box<Algo>)>::new(),
-                |mut s, elt| -> HashSet<(Box<Algo>, Box<Algo>)> {
-                    let vc = c.get_all_variables();
-                    let velt = elt.get_all_variables();
-                    if !vc.is_subset(&velt) && !velt.is_subset(&vc) {
-                        s.insert((c.clone(), elt.clone()));
-                    }
-                    s
-                },
-            ))
-            .cloned()
-            .collect();
-
-        // Update g and p
-        *g = gprim;
-        *p = uprim;
-    }
+    //Form new pairs with the algos such as the variables of one are not a subset of the other 
+    let pairs = g.iter().flat_map(|a| {
+        if c.get_all_variables().is_subset(&a.get_all_variables()) ||
+            a.get_all_variables().is_subset(&c.get_all_variables()) {
+            None
+        } else {
+            Some((c.clone(), a.clone()))
+        }
+    });
+    //Insert the new pairs in p
+    p.extend(pairs);
 }
 
 #[cfg(test)]
